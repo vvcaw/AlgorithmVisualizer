51765ac72afd80569b9ca27818064353398cffb4
Eric Elflein <57096338+vvcaw@users.noreply.github.com>
2022-01-19 20:21:41 +0100

    Setup

---
+++ src/app.jsx
@@ -0,0 +1,20 @@
+import { Logo } from './pages/logo'
+
+export function App(props) {
+  return (
+    <>
+      <Logo />
+      <p>Hello Vite + Preact!</p>
+      <p>
+        <a
+          class="bg-black"
+          href="https://preactjs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Preact
+        </a>
+      </p>
+    </>
+  )
+}
