4dcb8c2dbb6c31c0ad6f7a5487679a9358592fb0
Eric Elflein <57096338+vvcaw@users.noreply.github.com>
2022-01-21 00:13:11 +0100

    Added run box example

---
+++ src/components/algorithms/bubble_sort/BubbleSort.jsx
@@ -0,0 +1,15 @@
+import {BubbleSortRun} from './BubbleSortRun'
+
+export function BubbleSort(props) {
+    return (
+        <>
+            <div className="p-4 w-full h-2/4 rounded-md bg-white shadow-md">
+
+            </div>
+
+            <div className="p-4 w-full h-28 rounded-md bg-primary shadow-md">
+                <BubbleSortRun/>
+            </div>
+        </>
+    )
+}
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,12 @@
+import {BubbleSort} from "../algorithms/bubble_sort/BubbleSort";
+
 export function Main(props) {
     return (
-        <div className={`bg-background h-screen ${props.col}`}></div>
+        <>
+            <div className={`bg-background w-full flex flex-col p-4 gap-y-4 gap-x-4 ${props.col}`}>
+                {/*Container for active algorithm*/}
+                <BubbleSort/>
+            </div>
+        </>
     )
 }
