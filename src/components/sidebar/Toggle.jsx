3541deaf4d297915a220bdbd34fefdc47e20e6dd
Eric Elflein <57096338+vvcaw@users.noreply.github.com>
2022-01-23 17:35:18 +0100

    rebased to svelte

--- src/components/sidebar/Toggle.jsx
+++
@@ -1,8 +0,0 @@
-export const Toggle = (props) => (
-    <>
-        <button onClick={props.toggle}
-                className="w-14 h-14 border border-2 border-subfontcolor rounded-md flex justify-center items-center focus:bg-subfontcolor lg:hidden">
-            {props.children}
-        </button>
-    </>
-)
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({})
+export default defineConfig({
+  plugins: [svelte()]
+})
