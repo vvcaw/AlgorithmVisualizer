674eb9b8e2e8fd11601e52914f2431ac02791a7b
Eric Elflein <57096338+vvcaw@users.noreply.github.com>
2022-01-23 20:30:50 +0100

    Working mobile layout for sidebar

--- src/lib/sidebar/Toggle.svelte
+++ src/lib/sidebar/Toggle.svelte
@@ -1,12 +1,16 @@
 <script>
-    export let click
-    let toggled = false
+    import XIcon from "../icons/XIcon.svelte";
+    import MenuAlt4Icon from "../icons/MenuAlt4Icon.svelte";
+
+    export let toggled = false
+
+    const toggle = () => { toggled = !toggled }
 </script>
 
-<button class="w-14 h-14 border border-2 border-subfontcolor rounded-md flex justify-center items-center focus:bg-subfontcolor lg:hidden">
+<button on:click={toggle} class="w-14 h-14 border border-2 border-subfontcolor rounded-md flex justify-center items-center focus:bg-subfontcolor lg:hidden">
     {#if toggled}
-
+        <XIcon iconStyle="w-14 h-14 text-fontcolor"/>
     {:else}
-
+        <MenuAlt4Icon iconStyle="w-14 h-14 text-fontcolor"/>
     {/if}
 </button>
